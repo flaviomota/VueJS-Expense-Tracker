8cd2194b5583524a28a2e708c420bde4958aa8a8
Flávio Mota <flaviofilipemota@gmail.com>
2024-02-06 15:16:32 +0100

    Created and Imported components

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,27 @@
-<template>My App</template>
+<template>
+  <Header />
+  <div class="container">
+    <Balance />
+    <IncomeExpenses />
+    <TransactionsList />
+    <AddTransaction />
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue';
+import Balance from './components/Balance.vue';
+import IncomeExpenses from './components/IncomeExpenses.vue';
+import TransactionsList from './components/TransactionList.vue';
+import AddTransaction from './components/AddTransaction.vue';
+
+export default {
+  components: {
+    Header,
+    Balance,
+    IncomeExpenses,
+    TransactionsList,
+    AddTransaction,
+  },
+};
+</script>
